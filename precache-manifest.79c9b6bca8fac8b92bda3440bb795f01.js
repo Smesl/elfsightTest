c94aa98a54f5cdaeec7dc33f244c8434a0e8cfb7
Smesl <maks.mazu@mail.ru>
2020-10-09 16:19:03 +0300

    Updates

---
+++ precache-manifest.79c9b6bca8fac8b92bda3440bb795f01.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63887febea0d7138da149688cac5e05",
+    "url": "/elfsightTest/index.html"
+  },
+  {
+    "revision": "8f5bc190fcade8a7d0c1",
+    "url": "/elfsightTest/static/css/main.22fdc546.chunk.css"
+  },
+  {
+    "revision": "b17b756754377da11f22",
+    "url": "/elfsightTest/static/js/2.f4f1dcdc.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/elfsightTest/static/js/2.f4f1dcdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5bc190fcade8a7d0c1",
+    "url": "/elfsightTest/static/js/main.3d294a0b.chunk.js"
+  },
+  {
+    "revision": "cb6fa6d85e42a305ec22",
+    "url": "/elfsightTest/static/js/runtime-main.9fabc956.js"
+  },
+  {
+    "revision": "2e91137a4a365776d43602f262c2d108",
+    "url": "/elfsightTest/static/media/addInfo.2e91137a.svg"
+  },
+  {
+    "revision": "d6ffedd2314274a92b6fc3caf743b153",
+    "url": "/elfsightTest/static/media/analyzes.d6ffedd2.svg"
+  },
+  {
+    "revision": "95804ba38ea12bd3a70da90ef85cfe8d",
+    "url": "/elfsightTest/static/media/arrow-left.95804ba3.svg"
+  },
+  {
+    "revision": "5d3b5e73bff6e632bbf0aa1c47d5ca98",
+    "url": "/elfsightTest/static/media/doctors.5d3b5e73.svg"
+  },
+  {
+    "revision": "af0e0c68968657cc9b1124ba46395059",
+    "url": "/elfsightTest/static/media/girl.af0e0c68.svg"
+  },
+  {
+    "revision": "0d5f1085931899693f7dc801a6c46e5e",
+    "url": "/elfsightTest/static/media/help.0d5f1085.svg"
+  },
+  {
+    "revision": "1e7679212bba790bdaaeab35eecfc008",
+    "url": "/elfsightTest/static/media/helpfull.1e767921.svg"
+  },
+  {
+    "revision": "06d316417dc5df8f31cc79a2b42e0736",
+    "url": "/elfsightTest/static/media/history.06d31641.svg"
+  },
+  {
+    "revision": "c0cfdf18479278d475bb1ba2cb181727",
+    "url": "/elfsightTest/static/media/infoPetient.c0cfdf18.svg"
+  },
+  {
+    "revision": "7bdb43299a73d84e04578a7dd3091f5b",
+    "url": "/elfsightTest/static/media/left.7bdb4329.svg"
+  },
+  {
+    "revision": "a4c6a8989ed8383b29ba339c7743597a",
+    "url": "/elfsightTest/static/media/look.a4c6a898.svg"
+  },
+  {
+    "revision": "b8c130e8983d2e127482bf30ebe3b5e8",
+    "url": "/elfsightTest/static/media/man.b8c130e8.svg"
+  },
+  {
+    "revision": "f48047cf68ce842d99a9502f4e50fc7b",
+    "url": "/elfsightTest/static/media/message.f48047cf.svg"
+  },
+  {
+    "revision": "11ecc6cfe49effa981e7d36e7bb98dec",
+    "url": "/elfsightTest/static/media/notifications.11ecc6cf.svg"
+  },
+  {
+    "revision": "397918467f4c68f08847b7d1b1a09717",
+    "url": "/elfsightTest/static/media/profile.39791846.svg"
+  },
+  {
+    "revision": "0c2ef03c445c8476829adc9cfc4034db",
+    "url": "/elfsightTest/static/media/right.0c2ef03c.svg"
+  },
+  {
+    "revision": "622cc86730d840534cae6f52207e5168",
+    "url": "/elfsightTest/static/media/search.622cc867.svg"
+  },
+  {
+    "revision": "22dae7c46737b6bb27c7c3c39911a2aa",
+    "url": "/elfsightTest/static/media/select.22dae7c4.svg"
+  },
+  {
+    "revision": "8fc2b05f46010c34cd7b1c0741107145",
+    "url": "/elfsightTest/static/media/test.8fc2b05f.svg"
+  },
+  {
+    "revision": "d13353595edb81fef2a2fa076a0851aa",
+    "url": "/elfsightTest/static/media/velox.d1335359.svg"
+  },
+  {
+    "revision": "60a46b9f1e0d17800d8d63ebfc6f644a",
+    "url": "/elfsightTest/static/media/woman.60a46b9f.svg"
+  }
+]);
